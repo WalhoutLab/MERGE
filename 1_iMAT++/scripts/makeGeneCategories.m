f68dc3d435714cedf56f0d086a7dfdd1c64e5d64
Xuhang Li <Xuhang.Li@umassmed.edu>
2020-03-16 14:53:15 -0400

    gene category buider updated

--- 1_iMAT++/scripts/makeGeneCategories.m
+++ 1_iMAT++/scripts/makeGeneCategories.m
@@ -1,37 +1,27 @@
-% make the initial category of seq data
-% the criterias are: 5% FDR, 2 fold change cutoff or no FC cutoff
+%% Overview
+% This is an interactive gene category builder that helps user to fit and
+% build the category input for iMAT++. The expression quantifications (TPM
+% or FPKM) could be provided as a csv table.
+%% part I: load the expression data and adjust the format
+TPM = readtable('./../inputs/TPM.csv');
+% (specific to worm model)
+% worm expression data are often labeled with WormBase ID (WBID). So, we
+% provided a simple lookup table for iCEL1314.
 
-%% part I: process the differential expression data
-% because DE analysis doesnt apply to this dataset, so we change the
-% strategy to picking the top 
-% the original rowwise data is more normally distributed (more match CLT)
-% (data not shown), while the rowwise log2(TPM+1) is more skewed. we use
-% the z-transformation of the TPM matrix (rowwise Z) to determine if a gene
-% is significantly differential expressed.
-zCutoff = 2.33; % p < 0.01
-load('TPM.mat')
-expMat = cell2mat(TPM(2:end,2:end));
-TPM = cell2table(TPM(2:end,:),'VariableNames',TPM(1,:));
-zMat = normalize(expMat,2);
-DEgenes = TPM.Gene(max(abs(zMat),[],2)>zCutoff);
-% in the refinement, a gene with z score higher than cutoff will be upgrade
-% to high if necessary;
-save('DElist.mat','DEgenes','zMat');
+% for other models or inputs, please make sure:
+% 1. the columns are conditions to be analyzed
+% 2. the rows are genes with gene ID used in the model
 %% part II: the classification based on absolute value
-% only metabolic genes
+% user can perform the fitting both by all genes or only metabolic genes
 fitData = TPM{:,2:end};
-%fitData = [data.rep4_daf16;data.rep4_daf2;data.rep4_double;data.rep4_N2;data.rep5_daf16;data.rep5_daf2;data.rep5_double;data.rep5_N2;data.rep6_daf16;data.rep6_daf2;data.rep6_double;data.rep6_N2];
 histogram(log2(fitData))
 % fit bimodel guassian
 rng(1126)
-x = log2(fitData);
+x = log2(fitData);% this automatically ignored all the zeros
 x(isinf(x)) = [];
 fit = fitgmdist(x',2,'Options',statset('Display','final','MaxIter',3000,'TolFun',1e-9),'Replicates',10);
 % note: the sigma in output is sigma^2
-%% visualization
-%options = statset('MaxIter',300, 'MaxFunEvals',600);
-%paramEsts = mle(x, 'pdf',pdf_normmixture, 'start',start, ...
-%                          'lower',lb, 'upper',ub, 'options',options)
+%% visualization of the guassian distribution fitted
 bins = -15:.5:15;
 h = bar(bins,histc(x,bins)/(length(x)*.5),'histc');
 h.FaceColor = [.9 .9 .9];
@@ -43,14 +33,14 @@ plot(xgrid,pdfgrid,'-')
 hold off
 xlabel('x')
 ylabel('Probability Density')
-%%
+%% label the desired cutoff
 xline( fit.mu(2)-sqrt(fit.Sigma(2))*3,'--r');
 xline(fit.mu(1)-sqrt(fit.Sigma(1))*3,'--r');
 % xline(fit.mu(3) + sqrt(fit.Sigma(3)),'--r');
 % xline(-1.71,'--r');
 xline(min(x),'--r');
 
-%% gene catagorization
+%% build the gene catagories
 zero2low = min(x);%fit.mu(1)-sqrt(fit.Sigma(1))*3;%-sqrt(fit.Sigma(1))*2;%considering the absolute TPM is still high for mu1, we use u1-2sigma to define zero category
 dynamic2high = fit.mu(2)-sqrt(fit.Sigma(2))*3;%very greedy for high, call all detectable met genes as high
 names = TPM.Properties.VariableNames(2:end);
