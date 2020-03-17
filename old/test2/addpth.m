afab916bc7993353ece76414182266aea92c11c3
Xuhang Li <Xuhang.Li@umassmed.edu>
2020-03-17 10:34:46 -0400

    clean up

--- old/test2/addpth.m
+++
@@ -1,3 +0,0 @@
-addpath ~/cobratoolbox/
-addpath /share/pkg/gurobi/810/linux64/matlab/
-addpath ~/new_matlab_functions/
--- old/test2/run.sh
+++
@@ -1,7 +0,0 @@
-module load gurobi/810
-module load matlab/R2017a
-module load perl/5.10.1
-module load git/2.9.5
-git config --global http.sslverify "false"
-matlab <./myFlux.m> makeFlux.log
-wait
