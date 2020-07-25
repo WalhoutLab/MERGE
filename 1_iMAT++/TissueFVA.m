12e13b87006b186c9cd4d0d11ce6a14f2d94dcc1
lsafak <lutfu.yilmaz@umassmed.edu>
2020-07-25 18:21:38 -0400

    Update TissueFVA.m

--- 1_iMAT++/TissueFVA.m
+++ 1_iMAT++/TissueFVA.m
@@ -1,5 +1,5 @@
-%% this is the main program to generate tissue FVA in TableS5
-%% load model
+%% This is the main program to generate tissue FVA in TableS5
+%% Load model
 addpath ~/cobratoolbox/
 addpath /share/pkg/gurobi/810/linux64/matlab/
 addpath ./../bins/
@@ -10,15 +10,15 @@ fprintf('C. elegans Tissue Model FVA Started... \n');
 fprintf('loading model and input parameters... \n');
 totalTime = tic();
 load('Tissue.mat');
-% the loaded model is already constrained with default constraints and
-% side/storage parameters. One can modify like following codes;
+% The loaded model is already constrained with default constraints and
+% side/storage parameters. To modify a reaction constraint (RCC0005 as an example reaction):
 % worm = changeRxnBounds(worm,'RCC0005',10,'l');
-% % change the side proportion
+% To change the side nutrient proportion:
 % worm.S(end-1, strcmp('EXC0050',worm.rxns)) = 0.01*9.6641833;%storage
 % worm.S(end, strcmp('EXC0050',worm.rxns)) = 0.01*9.6641833; %side
 
-% reset some constraints to make the model ready for integration 
-% release the input constraints for integration 
+% Reset some constraints to make the model ready for integration 
+% Release the input constraints for integration 
 model = changeRxnBounds(model,'EXC0050_L',-1000,'l');
 model = changeRxnBounds(model,'EX00001_E',-1000,'l');
 model = changeRxnBounds(model,'EX00007_E',-1000,'l');
@@ -35,17 +35,18 @@ model = changeRxnBounds(model,'DMN0033_X',0,'l');
 model = changeRxnBounds(model,'DMN0033_I',1,'u');
 model = changeRxnBounds(model,'DMN0033_X',1,'u');
 
-
 model = changeRxnBounds(model,'RMC0005_I',0,'l');
 model = changeRxnBounds(model,'RMC0005_X',0,'l');
 model = changeRxnBounds(model,'RMC0005_I',0,'u');
 model = changeRxnBounds(model,'RMC0005_X',0,'u');
+%
 % parseGPR takes hugh amount of time, so preparse and save result in model
 parsedGPR = GPRparser_xl(model);% Extracting GPR data from model
 model.parsedGPR = parsedGPR;
-%% load epsilon
-% epsilon is also supplied as a jason file. We convert it to a vector
-% variable that in the same order as model.rxns.
+
+%% Load flux thresholds (epsilons)
+% The epsilon values are also supplied in a jason file. We convert it to a vector
+% variable that follows the same order as model.rxns.
 fname = 'epsilon.json';
 str = fileread(fname);
 for i = 1:length(str)
@@ -66,12 +67,14 @@ for i = 1: length(fields)
         epsilon_r(strcmp(model.rxns,fields{i})) = epsilon.(fields{i})(1);
     end
 end
-%% define the par cluster
+
+%% Define the par cluster
 myCluster = parcluster('local');
 myCluster.NumWorkers = 128;
 saveProfile(myCluster);
 parpool(20,'SpmdEnabled',false); % we tested this program in a 20-core server
-%% do FVA for the six non-intestinal tissues.
+
+%% Do FVA for the six non-intestinal tissues.
 names = {'Body_wall_muscle','Glia','Gonad','Hypodermis','Neurons','Pharynx'};
 Xrxns = model.rxns; % we calculate the FVA of both X tissue and I tissue
 for i = 1:length(names)
@@ -85,7 +88,8 @@ for i = 1:length(names)
     save(['output/FVA/',names{i},'_speedmode_1.mat'],'myFVA');
     toc(fitTime);
 end
-%% now do the intestine integration
+
+%% Now do the intestine integration
 % first get the average flux distribution of all six non-intestinal
 % tissues.
 for i = 1:length(names)
@@ -97,12 +101,12 @@ fluxSum_OFD = zeros(length(model.rxns),1);
 for i = 1:length(names)
     eval(['fluxSum_OFD = ',names{i},'.OFD .* weights(i) + fluxSum_OFD;']);
 end
-% collapse the X compartment in the dual model to save computational power
+% Collapse the X compartment in the dual model to save computational power
 IntestineModel_OFD = collapseX(model,'X',fluxSum_OFD);
-% update parsed GPR
+% Update parsed GPR
 parsedGPR = GPRparser_xl(IntestineModel_OFD);% Extracting GPR data from model
 IntestineModel_OFD.parsedGPR = parsedGPR;
-% update the epsilon list
+% Update the epsilon list
 epsilon_new_f = 0.01 * ones(length(IntestineModel_OFD.rxns),1);
 epsilon_new_r = epsilon_new_f;
 for i = 1: length(IntestineModel_OFD.rxns)
@@ -114,11 +118,11 @@ end
 
 load('output/Intestine_speedmode_1.mat');
 
-% set free all X exchange
+% Set free all X exchange
 myCSM.MILP.ub(cellfun(@(x) ~isempty(regexp(x, '_X$','once')) ,IntestineModel_OFD.rxns)) = 1000;
 myCSM.MILP.lb(cellfun(@(x) ~isempty(regexp(x, '_X$','once')) ,IntestineModel_OFD.rxns)) = 0;
 
-% integration 
+% Integration 
 fprintf('now starting to calculate for %s... \n','Intestine');
 fitTime = tic();
 parforFlag=1;
@@ -132,5 +136,5 @@ toc(fitTime);
 fprintf('C. elegans Tissue Model FVA Completed! \n');
 toc(totalTime);
 
-% for generating the list of reactions to block, see the step #3 of
+%% For generating the list of reactions to block, see the step #3 of
 % walkthrough_large_scale_FVA.m
