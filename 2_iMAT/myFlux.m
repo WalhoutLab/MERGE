edd2474ab38d82484c05c0c8f7822360f57d1c20
WalhoutLab <lutfu.yilmaz@umassmed.edu>
2020-07-29 13:54:12 -0400

    Update myFlux.m

--- 2_iMAT/myFlux.m
+++ 2_iMAT/myFlux.m
@@ -1,4 +1,4 @@
-%% load model
+%% Load model
 addpath ./../bins/
 addpath ~/cobratoolbox/
 addpath /share/pkg/gurobi/810/linux64/matlab/
@@ -6,8 +6,8 @@ addpath ./../input/
 initCobraToolbox;
 load('Tissue.mat');
 
-% reset some constraints to make the model ready for integration 
-% release the input constraints for integration 
+% Reset some constraints to make the model ready for integration. 
+% Release the input constraints for integration. 
 model = changeRxnBounds(model,'EXC0050_L',-1000,'l');
 model = changeRxnBounds(model,'EX00001_E',-1000,'l');
 model = changeRxnBounds(model,'EX00007_E',-1000,'l');
@@ -24,16 +24,15 @@ model = changeRxnBounds(model,'DMN0033_X',0,'l');
 model = changeRxnBounds(model,'DMN0033_I',1,'u');
 model = changeRxnBounds(model,'DMN0033_X',1,'u');
 
-
 model = changeRxnBounds(model,'RMC0005_I',0,'l');
 model = changeRxnBounds(model,'RMC0005_X',0,'l');
 model = changeRxnBounds(model,'RMC0005_I',0,'u');
 model = changeRxnBounds(model,'RMC0005_X',0,'u');
-% parseGPR takes hugh amount of time, so preparse and integrate with model
+% parseGPR takes a significant amount of time, so preparse and integrate with model
 % here 
 parsedGPR = GPRparser_xl(model);% Extracting GPR data from model
 model.parsedGPR = parsedGPR;
-%% laod the gene expression data
+%% Load the gene expression data
 fname = 'geneCategories.json';
 str = fileread(fname);
 for i = 1:length(str)
@@ -44,9 +43,10 @@ end
 str = regexprep(str,'set(','');
 str = regexprep(str,')','');
 GeneExpression = jsondecode(str);
-% we use a structure variable to store the category information
-% now, make expression set for six tissues
-% hypodermis
+% We use a structure variable to store the category information.
+% Now, make expression set for six tissues.
+
+% Hypodermis
 GeneExpression.Hypodermis.high = strcat(GeneExpression.Hypodermis.high,'_X');
 GeneExpression.Hypodermis.lenient = strcat(GeneExpression.Hypodermis.lenient,'_X');
 GeneExpression.Hypodermis.low = strcat(GeneExpression.Hypodermis.low,'_X');
@@ -61,6 +61,7 @@ ExpCatag.low = [GeneExpression.Hypodermis.low;GeneExpression.Intestine.low];
 ExpCatag.zero = [GeneExpression.Hypodermis.zero;GeneExpression.Intestine.zero];
 ExpCatag.dynamic = setdiff(model.genes, unique([ExpCatag.high;ExpCatag.low;ExpCatag.zero]));
 ExpCatag_Hypodermis = ExpCatag;
+
 % Gonad
 GeneExpression.Gonad.high = strcat(GeneExpression.Gonad.high,'_X');
 GeneExpression.Gonad.lenient = strcat(GeneExpression.Gonad.lenient,'_X');
@@ -72,6 +73,7 @@ ExpCatag.low = [GeneExpression.Gonad.low;GeneExpression.Intestine.low];
 ExpCatag.zero = [GeneExpression.Gonad.zero;GeneExpression.Intestine.zero];
 ExpCatag.dynamic = setdiff(model.genes, unique([ExpCatag.high;ExpCatag.low;ExpCatag.zero]));
 ExpCatag_Gonad = ExpCatag;
+
 % Glia
 GeneExpression.Glia.high = strcat(GeneExpression.Glia.high,'_X');
 GeneExpression.Glia.lenient = strcat(GeneExpression.Glia.lenient,'_X');
@@ -83,6 +85,7 @@ ExpCatag.low = [GeneExpression.Glia.low;GeneExpression.Intestine.low];
 ExpCatag.zero = [GeneExpression.Glia.zero;GeneExpression.Intestine.zero];
 ExpCatag.dynamic = setdiff(model.genes, unique([ExpCatag.high;ExpCatag.low;ExpCatag.zero]));
 ExpCatag_Glia = ExpCatag;
+
 % Pharynx
 GeneExpression.Pharynx.high = strcat(GeneExpression.Pharynx.high,'_X');
 GeneExpression.Pharynx.lenient = strcat(GeneExpression.Pharynx.lenient,'_X');
@@ -94,6 +97,7 @@ ExpCatag.low = [GeneExpression.Pharynx.low;GeneExpression.Intestine.low];
 ExpCatag.zero = [GeneExpression.Pharynx.zero;GeneExpression.Intestine.zero];
 ExpCatag.dynamic = setdiff(model.genes, unique([ExpCatag.high;ExpCatag.low;ExpCatag.zero]));
 ExpCatag_Pharynx = ExpCatag;
+
 % Body_wall_muscle
 GeneExpression.Body_wall_muscle.high = strcat(GeneExpression.Body_wall_muscle.high,'_X');
 GeneExpression.Body_wall_muscle.lenient = strcat(GeneExpression.Body_wall_muscle.lenient,'_X');
@@ -105,6 +109,7 @@ ExpCatag.low = [GeneExpression.Body_wall_muscle.low;GeneExpression.Intestine.low
 ExpCatag.zero = [GeneExpression.Body_wall_muscle.zero;GeneExpression.Intestine.zero];
 ExpCatag.dynamic = setdiff(model.genes, unique([ExpCatag.high;ExpCatag.low;ExpCatag.zero]));
 ExpCatag_Body_wall_muscle = ExpCatag;
+
 % Neurons
 GeneExpression.Neurons.high = strcat(GeneExpression.Neurons.high,'_X');
 GeneExpression.Neurons.lenient = strcat(GeneExpression.Neurons.lenient,'_X');
@@ -116,6 +121,7 @@ ExpCatag.low = [GeneExpression.Neurons.low;GeneExpression.Intestine.low];
 ExpCatag.zero = [GeneExpression.Neurons.zero;GeneExpression.Intestine.zero];
 ExpCatag.dynamic = setdiff(model.genes, unique([ExpCatag.high;ExpCatag.low;ExpCatag.zero]));
 ExpCatag_Neurons = ExpCatag;
+
 % Intestine
 ExpCatag = struct();
 ExpCatag.high = [GeneExpression.Intestine.high];
@@ -123,7 +129,8 @@ ExpCatag.low = [GeneExpression.Intestine.low];
 ExpCatag.zero = [GeneExpression.Intestine.zero];
 ExpCatag.dynamic = setdiff(model.genes, unique([ExpCatag.high;ExpCatag.low;ExpCatag.zero]));
 ExpCatag_Intestine = ExpCatag;
-%merge all 
+
+%Combine the results 
 tissues = struct();
 tissues.Hypodermis = ExpCatag_Hypodermis;
 tissues.Gonad = ExpCatag_Gonad;
@@ -133,7 +140,8 @@ tissues.Body_wall_muscle = ExpCatag_Body_wall_muscle;
 tissues.Neurons = ExpCatag_Neurons;
 tissues.Intestine = ExpCatag_Intestine;
 
-%% load epsilon
+
+%% Load epsilon
 fname = 'epsilon.json';
 str = fileread(fname);
 for i = 1:length(str)
@@ -154,7 +162,11 @@ for i = 1: length(fields)
         epsilon_r(strcmp(model.rxns,fields{i})) = epsilon.(fields{i})(1);
     end
 end
-%% make PFD
+
+
+%% Integrate
+
+%Non-intestinal tissues
 names = fieldnames(tissues);
 names = names(1:end-1);
 for i = 1:length(names)
@@ -172,18 +184,19 @@ for i = 1:length(names)
     eval([names{i},' = myCSM;']);
     toc(t1)
  end
-%% now do the intestine integration
-% first get the average flux distribution
+ 
+%Intestine
+%First get the average flux distribution
 weights = [0.16481752424244808;0.19668335945561571;0.029394841229723516;0.055442983787911716;0.35973476877630634;0.19392652250799472];%needs to order by tissuesname
 fluxSum_PFD = zeros(length(model.rxns),1);
 for i = 1:length(names)
     eval(['fluxSum_PFD = ',names{i},'.PFD .* weights(i) + fluxSum_PFD;']);
 end
 IntestineModel_PFD = collapseX(model,'X',fluxSum_PFD);
-%update GPR
+%Update GPR
 parsedGPR = GPRparser_xl(IntestineModel_PFD);% Extracting GPR data from model
 IntestineModel_PFD.parsedGPR = parsedGPR;
-% update the epsilon list
+% Update the epsilon list
 epsilon_new_f = 0.01 * ones(length(IntestineModel_PFD.rxns),1);
 epsilon_new_r = epsilon_new_f;
 for i = 1: length(IntestineModel_PFD.rxns)
@@ -192,7 +205,7 @@ for i = 1: length(IntestineModel_PFD.rxns)
         epsilon_new_r(i) = epsilon_r(ismember(model.rxns,IntestineModel_PFD.rxns(i)));
     end
 end
-% integration 
+% Integration 
 fprintf('now starting to fit %s... \n','Intestine');
 t1 = tic();
 ExpCatag = tissues.Intestine;
@@ -202,8 +215,6 @@ ATPm = 10;
 doMinPFD = 1;
 type = 'canonical';
 myCSM = struct(); %my context specific model
-%PFD fitting
 [myCSM.PFD,myCSM.N_highFit,myCSM.N_zeroFit,myCSM.minLow,myCSM.minTotal,myCSM.OpenGene,myCSM.wasteDW,myCSM.HGenes,myCSM.RLNames,myCSM.MILP] = autoIntegration_iMAT(IntestineModel_PFD,storeProp,SideProp,epsilon_new_f,epsilon_new_r, ATPm, ExpCatag,doMinPFD,type);
-%OFD fitting
 save('output/Intestine.mat','myCSM');
 toc(t1)
